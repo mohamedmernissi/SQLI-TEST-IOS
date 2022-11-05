5ca434d71b9ff6ee906aa4346b5f1e1363723734
m.mernissi <m.mernissi@revotrends.com>
2022-11-05 10:25:48 +0100

    Initial Commit

---
+++ SQLI-TEST/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SQLI-TEST
+//
+//  Created by mohamed mernissi on 5/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SQLI-TESTUITests/SQLI_TESTUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SQLI_TESTUITestsLaunchTests.swift
+//  SQLI-TESTUITests
+//
+//  Created by mohamed mernissi on 5/11/2022.
+//
+
+import XCTest
+
+class SQLI_TESTUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
